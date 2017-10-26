ed3a83dc323e0206f240d9b4569ea8b65bdf3d79
Markus Flodmark <markus.flodmark@gmail.com>
2017-10-26 11:04:06 +0200

    Added DataAcces + connection string

---
+++ DAL/DataAccess.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook.DAL
+{
+    class DataAccess
+    {
+        private SqlConnection con;
+
+        public DataAccess()
+        {
+            var connectionString = Properties.Settings.Default.AddressBookConnection;
+            con = new SqlConnection(connectionString);
+        }
+
+        public DataSet ExecuteSelectCommand(string cmdText, CommandType cmdType)
+        {
+            var dataSet = new DataSet();
+            var dataTable = new DataTable();
+
+            var cmd = con.CreateCommand();
+
+            cmd.CommandType = cmdType;
+            cmd.CommandText = cmdText;
+            //cmd.Parameters.AddRange(param);
+
+            try
+            {
+                con.Open();
+
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    da.Fill(dataTable);
+                }
+
+                dataSet.Tables.Add(dataTable);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                cmd.Dispose();
+                con.Close();
+            }
+
+            return dataSet;
+
+
+        }
+    }
+
+   
+}
